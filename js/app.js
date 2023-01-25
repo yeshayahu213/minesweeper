9950054b2415b0a26b179f7e0cbc4497f08e43b9
yeshayahu213 <123544431+yeshayahu213@users.noreply.github.com>
2023-01-25 20:50:25 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ function onInitGame() {
   const elmokesh =document.querySelector('.MOKESH')
   console.log(elmokesh);
   elmokesh.innertext='mokesh'
-  
+ 
 
 }
 
@@ -58,42 +58,30 @@ function renderBoard(board) {
     var a=board[i].length
     for (var j = 0; j < a; j++) {
       console.log('h');
-    
+     
       var cellClass = getClassName({ i: i, j: j }) + ' '
 
-      for (var i = 0; i < board.length; i++) {
-        
-        for (var j = 0; j < board[i].length; j++) {
-        
-      for (var h = i- 1; h <= i + 1; h++) {
-        if (h < 0 || h >= board.length) continue;
-        for (var k = j - 1; k <= j + 1; k++) {
-          if (k < 0 || k>= board[i][j].length) continue;
-          else if (board[i][j].gameElement === MOKESH) board[i][j].minesAroundCount++
-         
-      }}}}}
-    
-    if (board[i][j].gameElement==='floor') {
-      cellClass += 'floor'
-    }
-      else{
-        cellClass += MOKESH
-      }
-      strHTML += `<td class="cell ${cellClass}" >`
-      }
-
-      strHTML += '</tr>'
-
       
-   
-    
-    const elBoard = document.querySelector('.board')
-    elBoard.innerHTML = strHTML
-   
-    // elmokesh.innertext='#'
-  }
+          if (board[i][j].gameElement==='floor') {
+            cellClass += 'floor'
+          }
+            else{
+              cellClass += 'MOKESH'
+            }
 
+
+
+            strHTML += `<td class="cell ${cellClass}"  onclick="onCellClicked(${i},${j})" >`
+         
+    }
   
+    strHTML += '</tr>' 
+  }
+    
+  const elBoard = document.querySelector('.board')
+  elBoard.innerHTML = strHTML
+}
+    
   
 
 
@@ -105,20 +93,50 @@ function getClassName(location) {
 }
 
 function renderCell(location, value) {
-  const cellSelector = '.' + getClassName(location) // cell-i-j
-  const elCell = document.querySelector(cellSelector)
+  // const cellSelector =  getClassName(location) // cell-i-j
+  // console.log(location);
+  const elCell = document.querySelector(location)
+  console.log(elCell);
   elCell.innerHTML = value
 }
 
-function countGamerNegs() {
-  var negsCount = 0;
 
-    
-    
+
+function onCellClicked( i, j)  {
+  console.log(i,j);
+  console.log(gBoard[i][j])
+  setMinesNegsCount(i,j)
+if (gBoard[i][j].gameElement===MOKESH) {
+  console.log('alan alan');
+  checkGameOver()
 }
+ if (gBoard[i][j].minesAroundCount===0){
+  console.log('h');
+ 
+ console.log(gBoard[i][j])   ;
 
-function onCellClicked(elCell, i, j)  {
+}
+else if (gBoard[i][j].minesAroundCount===1) {
+  console.log('hi');
+ 
+ console.log(gBoard[i][j])   ;
+
+}
+var arr=setMinesNegsCount(i, j)
+console.log(arr);
+for (var  i = 0; i < arr.length; i++) {
+  var strhtmk=''
+   strhtmk+='.cell'
+   var a=arr[i].h
+
+
+
+
+  renderCell('.cell' + a,MOKESH_IMG)
   
+}
+ 
+
 }
 
 function onCellMarked(elCell) {
@@ -126,9 +144,36 @@ function onCellMarked(elCell) {
 }
 
 function checkGameOver(){
+  alert('game over mf')
 
 } 
 
 function expandShown(board, elCell, i, j) {
   
+}
+function setMinesNegsCount(i,j) {
+  gBoard[i][j].minesAroundCount=0
+  var revalArray=[]
+  
+  for (var h = i- 1; h <= i + 1; h++) {
+    
+    if (h < 0 || h >= gBoard.length) continue;
+    for (var k = j - 1; k <= j + 1; k++) {
+      if (k < 0 || k>= gBoard[i].length) continue;
+      console.log(gBoard[i][j].minesAroundCount);
+      console.log(h,k)
+      revalArray.push({h,k})
+      if (gBoard[h][k].gameElement === MOKESH) {
+       
+        gBoard[i][j].minesAroundCount++
+       
+        
+
+      }
+      
+    }
+      return revalArray
+    } 
+
+      
 }
